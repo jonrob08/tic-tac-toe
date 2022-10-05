4f6bc469d36ee2e3883e301010e1d9501ae6068f
jonrob <jonrob08@gmail.com>
2022-10-04 19:37:31 -0700

    functional component add

---
+++ src/Game.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Board from "./Board";
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          history: [{
+            squares: Array(9).fill(null),
+          }],
+          xIsNext: true,
+        };
+      }
+
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+      return (
+        <div className="game">
+          <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Game;
+  
